ea44dc0b8fdeea5b61b95809898782d018fce2cd
MustafaGuer <mustafaguer@outlook.com>
2023-03-11 13:04:42 +0100

    fix: hour calculation

--- src/components/ServerRow.tsx
+++ src/components/ServerRow.tsx
@@ -5,10 +5,9 @@ const Server = (props: Result) => {
   const year = date.getFullYear();
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
   const day = ("0" + date.getDate()).slice(-2);
-  let hours = ("0" + (date.getHours() + 1)).slice(-2);
+  const hours = ("0" + (date.getHours())).slice(-2);
   const minutes = ("0" + date.getMinutes()).slice(-2);
   const seconds = ("0" + date.getSeconds()).slice(-2);
-  if (hours === "24") hours = "00";
 
   return (
     <tr>
